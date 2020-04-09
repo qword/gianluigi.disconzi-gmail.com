26f34cd5fb0827d963274978dd9eb81591dc6962
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2020-04-09 16:48:28 +0200

    Add interfaces for services

---
+++ pet-clinic-data/src/main/java/qword/spring/petclinic/service/PetService.java
@@ -0,0 +1,12 @@
+package qword.spring.petclinic.service;
+
+import qword.spring.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/qword/spring/petclinic/service/VetService.java
@@ -0,0 +1,12 @@
+package qword.spring.petclinic.service;
+
+import qword.spring.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
