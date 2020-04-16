8cd983c216357bdf230aae63f93b159a28080958
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2020-04-16 16:26:14 +0200

    Implement Map based services

---
+++ pet-clinic-data/src/main/java/qword/spring/petclinic/service/map/OwnerMapService.java
@@ -0,0 +1,34 @@
+package qword.spring.petclinic.service.map;
+
+import qword.spring.petclinic.model.Owner;
+import qword.spring.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/qword/spring/petclinic/service/map/PetMapService.java
@@ -0,0 +1,34 @@
+package qword.spring.petclinic.service.map;
+
+import qword.spring.petclinic.model.Pet;
+import qword.spring.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
